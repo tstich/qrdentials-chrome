ef23ca93e75e2234c8a4d98109f7a460aadaeac7
Timo Stich <mail@timostich.de>
2013-03-06 15:01:11 +0100

    Getting tab in popup itself now. Fixes #2

--- popup.js
+++ popup.js
@@ -1,14 +1,15 @@
 var baseURL = 'http://api.qrdentials.com:8080/iDentityServlet/';
 var pollPolicy = null;
 var startTime = 0;
-var url;
+var selectedTab = null;
+var keyWords = null;
 
 function create_qrcode(text) {
 	var qr = qrcode(6, 'M'); // 864 Bits max, see http://blog.qr4.nl/page/QR-Code-Data-Capacity.aspx
 	qr.addData(text);
 	qr.make();
 	return qr.createImgTag(4);
-};
+}
 
 function delete_cookie(next) {
     chrome.cookies.remove({url:baseURL,name:'JSESSIONID'}, function(cookie) {
@@ -16,16 +17,53 @@ function delete_cookie(next) {
     });
 }
 
+function get_tab(next) {
+	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  		selectedTab = tabs[0];
+  		next();
+	});
+}
+
+function Uint8ArrayToCryptoWords(srcArray) {
+  var words = [];
+  var wordLength = 0;
+  for (var i = 0; i < srcArray.length; i = i + 4) {
+    words[wordLength] = (srcArray[i] << 24) | (srcArray[i+1] << 16) | (srcArray[i+2] << 8) | (srcArray[i+3]);
+    wordLength = wordLength + 1;
+  };
+  return CryptoJS.lib.WordArray.create(words, srcArray.length);
+}
+
+function randomKey() {
+  var keyBytes = new Uint8Array(16);
+  window.crypto.getRandomValues(keyBytes);
+  return Uint8ArrayToCryptoWords(keyBytes);
+}
+
+function login(binaryResponse) {
+  // Decrypt
+  var cypherWords = Uint8ArrayToCryptoWords(binaryResponse);
+  var iv  = CryptoJS.enc.Hex.parse('00000000000000000000000000000000');
+
+  var decryptedWords = CryptoJS.AES.decrypt(CryptoJS.enc.Base64.stringify(cypherWords), keyWords, { iv: iv });
+  var decryptedText = CryptoJS.enc.Utf8.stringify(decryptedWords);
+
+  // Send credentials to content script
+  var credentials = JSON.parse(decryptedText);
+  chrome.tabs.sendMessage(selectedTab.id, credentials, function() {});
+}
 
 function setup_popup(withSession) {
-	var urlParts = chrome.extension.getBackgroundPage().selectedURL.split('/');
+	var urlParts = selectedTab.url.split('/');
 	var url = urlParts[0] + '//' + urlParts[2];
 
 	if( withSession ) {
 	    chrome.cookies.get({url:baseURL,name:'JSESSIONID'}, function(cookie) {
 		    var sessionID = cookie.value;
-			//document.getElementById('qr').innerHTML = 'Random: ' + chrome.extension.getBackgroundPage().randomKey();
-			document.getElementById('qr').innerHTML = create_qrcode(url + '?s=' + sessionID + '&k=' + chrome.extension.getBackgroundPage().randomKey());
+		    keyWords = randomKey();
+		    var qrMessage = url + '?s=' + sessionID + '&k=' + CryptoJS.enc.Hex.stringify(keyWords);
+			//document.getElementById('qr').innerHTML = qrMessage;
+			document.getElementById('qr').innerHTML = create_qrcode(qrMessage);
 	    });		
 	} else {
 		//document.getElementById('qr').innerHTML = 'QRData: ' + url + withSession;
@@ -47,7 +85,7 @@ function poll_command() {
 				var arraybuffer = pollCommand.response;
 				if( arraybuffer ) {
 					var binaryResponse = new Uint8Array(arraybuffer);
-					chrome.extension.getBackgroundPage().login(binaryResponse);
+					login(binaryResponse);
 					delete_cookie(window.close);
 				} else {
 					setTimeout(poll_command, pollPolicy.interval);
@@ -61,9 +99,8 @@ function poll_command() {
 }
 
 function new_command() {
-
 	var newCommand = new XMLHttpRequest();
-	newCommand.open('GET', baseURL + 'new.json?url=dummy', true);
+	newCommand.open('GET', baseURL + 'new.json?url=' + encodeURIComponent(selectedTab.url), true);
 	newCommand.onreadystatechange =  function() {
 		if( newCommand.readyState == 4 ) {
 			// Got Response From Server
@@ -86,5 +123,5 @@ function new_command() {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-	delete_cookie(new_command);
+	delete_cookie(get_tab(new_command));
 });
