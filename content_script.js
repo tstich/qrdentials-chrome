1ee7d07d21d3ef4c833a68b041c031988fca0b81
Timo Stich <mail@timostich.de>
2013-03-10 18:09:48 +0100

    New test framework for the Login Form parser. Fixes #5.

--- content_script.js
+++ content_script.js
@@ -1,30 +1,24 @@
-// Search for login
-var passwordForm = $("form:has(:password:visible):first");
-var passwordFields;
-var usernameField;
+var loginForm = new LoginForm();
 
-if( passwordForm.length > 0 ) {
-  passwordFields = passwordForm.find(":password:visible");
-  passwordFields.css("background","red");
-  
-  if( passwordFields.length == 1 ) {
-   usernameField = passwordForm.find(":text:visible:first");
-   usernameField.css("background","green");
-  }
+loginForm.find('*');
+
+if( loginForm.passwordForm.length == 1) {
+
+  loginForm.passwordFields.css('background', 'red');
+  loginForm.usernameField.css('background', 'green');
 
   //Signal Backend to enable the page action
-  chrome.extension.sendMessage({type:'enablePageAction'}, function() {});
+  chrome.extension.sendMessage({type:'enablePageAction'}, function() {});	
 }
-
 
 chrome.extension.onMessage.addListener(function(message, sender, response) 
 {
 	if( message.type == 'page_type' ) {
 		var type = 'n'; // Non-Login
-		if( passwordFields.length == 1 ) {
+		if( loginForm.passwordFields.length == 1 ) {
 			type = 'l'; // Login
 		}
-		if( passwordFields.length > 1 ) {
+		if( loginForm.passwordFields.length > 1 ) {
 			type = 'c'; // Change Password
 		}
 		response(type);
@@ -32,18 +26,18 @@ chrome.extension.onMessage.addListener(function(message, sender, response)
 
 	if( message.type == 'l' ) {
 		// Login
-		usernameField.val( message.username );
-		passwordFields.val( message.password );
+		loginForm.usernameField.val( message.username );
+		loginForm.passwordFields.val( message.password );
 
-		passwordForm.submit();
+		loginForm.passwordForm.submit();
 	}
 
 	if( message.type == 'c' ) {
 		// Change Password
-   		passwordInputs.eq(0).val(message.old_password);
-   		passwordInputs.slice(1,3).val(message.new_password);
+   		loginForm.passwordInputs.eq(0).val(message.old_password);
+   		loginForm.passwordInputs.slice(1,3).val(message.new_password);
 
-		passwordForm.submit();
+		loginForm.passwordForm.submit();
 	}
 
 });
