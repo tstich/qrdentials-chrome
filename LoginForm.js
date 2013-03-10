aeb8ab4a21f415e47a740989ce63e3595ac23a23
Timo Stich <mail@timostich.de>
2013-03-10 18:09:48 +0100

    Tests for input, password and submit working!

---
+++ LoginForm.js
@@ -0,0 +1,31 @@
+function LoginForm() {
+
+	var passwordForm;
+	var passwordFields;
+	var usernameField;
+	var submitButton;
+
+	this.find = function(root) {
+	// Search for login
+	this.passwordForm = $(root).find("form:has(:password):first");
+ 
+	if( this.passwordForm.length > 0 ) {
+  		this.passwordFields = this.passwordForm.find(":password");
+  
+	  if( this.passwordFields.length == 1 ) {
+  		 this.usernameField = this.passwordForm.find(":text:first");
+  		 if( this.usernameField.length == 0 ) {
+  		 	this.usernameField = this.passwordForm.find(":input").filter("[type=email]").first();
+  		 }
+ 		}
+
+ 		this.submitButton = this.passwordForm.find(":submit");
+  		 if( this.submitButton.length == 0 ) {
+  		 	this.submitButton = this.passwordForm.find(":input").filter("[onclick]").last();
+  		 }
+  		 if( this.submitButton.length == 0 ) {
+  		 	this.submitButton = this.passwordForm.find("a").filter("[href^=javascript]").last();
+  		 }
+	}
+}
+}
--- test/spec/LoginSpec.js
+++ test/spec/LoginSpec.js
@@ -1,24 +1,127 @@
+// IMPORTANT: The form in the html to be tested needs to be inside a <div>, otherwise the test framework doesn't work!
+
 describe("Parse Login Pages", function() {
   jasmine.getFixtures().fixturesPath = 'pages/';
 
   it("Kabel Deutschland", function() {
     var root = readFixtures('www.kabeldeutschland.de_portal.html');
 
-    var passwordForm = $(root).find("form:has(:password):first");
+    var loginForm = new LoginForm();
+    loginForm.find(root);
 
-    expect(passwordForm.length).toBe(1);
-    expect(passwordForm.attr('loginspec')).toBe('form');
-
-
-    var passwordFields = passwordForm.find(":password");
-    
-    expect( passwordFields.length ).toBe(1);
-    expect( passwordFields.attr('loginspec') ).toBe('password');
-
-    var usernameField = passwordForm.find(":text:first");
-    
-    expect( usernameField.length ).toBe(1);
-    expect( usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
   });
+
+  it("Comdirect", function() {
+    var root = readFixtures('kunde.comdirect.de_lp_wt_login.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
+  });
+
+  it("DKB", function() {
+    var root = readFixtures('banking.dkb.de_dkb_-.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
+  });
+
+  it("Amazon", function() {
+    var root = readFixtures('amazon.de_ap_sigin.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+  });
+
+
+  it("Schwab", function() {
+    var root = readFixtures('www.schwab.com_public_eac_home.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
+  });
+
+  it("NVIDIA Developer", function() {
+    var root = readFixtures('developer.nvidia.com_user_login.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
+  });
+
+  it("Commerzbank", function() {
+    var root = readFixtures('www.commerzbanking.de.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
+  });
+
+  it("AAB", function() {
+    var root = readFixtures('akp.aab.de.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
+  });
+
+  it("Github", function() {
+    var root = readFixtures('github.com_login.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
+  });
+
+  it("Facebook", function() {
+    var root = readFixtures('www.facebook.com.html');
+
+    var loginForm = new LoginForm();
+    loginForm.find(root);
+
+    expect( loginForm.passwordForm.attr('loginspec')).toBe('form');
+    expect( loginForm.passwordFields.attr('loginspec') ).toBe('password');
+    expect( loginForm.usernameField.attr('loginspec') ).toBe('user');
+    expect( loginForm.submitButton.attr('loginspec') ).toBe('submit');
+  });
+
 
 });
